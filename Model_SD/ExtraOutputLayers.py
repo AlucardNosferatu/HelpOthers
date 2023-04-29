a53eb6c0b54753f47b18dc5fca083a4e8f25f1c5
Ebenezer Scrooge <1641367382@qq.com>
2023-04-29 22:13:53 +0800

    Update

--- Model_SD/ExtraOutputLayers.py
+++ Model_SD/ExtraOutputLayers.py
@@ -8,24 +8,33 @@ from DL.Config_FCN import img_shape
 def build_output_block():
     # input_layer = tf.keras.Input(shape=(None, None, None))
     input_tensor = tf.keras.Input(shape=(int(img_shape[0] / 32), int(img_shape[1] / 32), 512))
-    x = tf.keras.layers.UpSampling2D(size=(4, 4), interpolation='bilinear')(input_tensor)
-    x = tf.keras.layers.Conv2D(64, (16, 16), padding='same')(x)
+
+    x = tf.keras.layers.UpSampling2D(size=(8, 8), interpolation='bilinear')(input_tensor)
+
+    x = tf.keras.layers.Conv2D(128, (32, 32), padding='same')(x)
     x = tf.keras.layers.BatchNormalization()(x)
-    x = tf.keras.layers.Activation('relu')(x)
-    x = tf.keras.layers.Conv2D(32, (4, 4), padding='same')(x)
+    x = tf.keras.layers.Activation('selu')(x)
+
+    x = tf.keras.layers.Conv2DTranspose(64, (32, 32), (2, 2), padding='same')(x)
     x = tf.keras.layers.BatchNormalization()(x)
-    x = tf.keras.layers.Activation('relu')(x)
-    x = tf.keras.layers.UpSampling2D(size=(4, 4), interpolation='bilinear')(x)
-    x = tf.keras.layers.Conv2D(16, (16, 16), padding='same')(x)
+    x = tf.keras.layers.Activation('selu')(x)
+
+    # x = tf.keras.layers.UpSampling2D(size=(4, 4), interpolation='bilinear')(x)
+
+    x = tf.keras.layers.Conv2D(32, (16, 16), padding='same')(x)
     x = tf.keras.layers.BatchNormalization()(x)
-    x = tf.keras.layers.Activation('relu')(x)
-    x = tf.keras.layers.Conv2D(8, (4, 4), padding='same')(x)
+    x = tf.keras.layers.Activation('selu')(x)
+
+    x = tf.keras.layers.Conv2DTranspose(16, (16, 16), (2, 2), padding='same')(x)
     x = tf.keras.layers.BatchNormalization()(x)
-    x = tf.keras.layers.Activation('relu')(x)
-    x = tf.keras.layers.UpSampling2D(size=(4, 4), interpolation='bilinear')(x)
-    x = tf.keras.layers.Conv2D(3, (4, 4), strides=(2, 2), padding='same')(x)
+    x = tf.keras.layers.Activation('selu')(x)
+
+    # x = tf.keras.layers.UpSampling2D(size=(4, 4), interpolation='bilinear')(x)
+
+    x = tf.keras.layers.Conv2D(3, (8, 8), strides=(1, 1), padding='same')(x)
     x = tf.keras.layers.BatchNormalization()(x)
     x = tf.keras.layers.Activation('sigmoid')(x)
+
     new_model = tf.keras.Model(inputs=input_tensor, outputs=x)
     return new_model
 
