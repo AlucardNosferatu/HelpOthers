c8deee8c8e3ab62f437a444aca94f3654bc0ad84
Ebenezer Scrooge <1641367382@qq.com>
2023-06-18 10:11:20 +0800

    Update PPMI.py

--- Data/GNN/PPMI.py
+++ Data/GNN/PPMI.py
@@ -13,13 +13,20 @@ def pmi(
     bi_gram_measures = BigramAssocMeasures()
     finder = BigramCollocationFinder.from_words(words)
     pmi_list = []
+    pair_dict = {}
     for row in finder.score_ngrams(bi_gram_measures.pmi):
+        word_pair = row[0]
         if vocab is not None:
-            word_pair = row[0]
             if word_pair[0] in vocab and word_pair[1] in vocab:
                 if word_pair[0] == word_pair[1]:
                     continue
+                elif word_pair[1] in pair_dict.keys():
+                    if word_pair[0] in pair_dict[word_pair[1]]:
+                        continue
             else:
                 continue
         pmi_list.append(row)
+        if word_pair[0] not in pair_dict.keys():
+            pair_dict.__setitem__(word_pair[0], [])
+        pair_dict[word_pair[0]].append(word_pair[1])
     return pmi_list
