58f3730a1eb69957dc1cf7081d827d0320db5a99
Ebenezer Scrooge <1641367382@qq.com>
2023-06-23 22:49:29 +0800

    Update ScorePredictor.py

--- Model/BertGCN/ScorePredictor.py
+++ Model/BertGCN/ScorePredictor.py
@@ -7,9 +7,14 @@ from Model.TextGCN.GNN import GraphConv
 from Model.TextGCN.ScorePredictor import data_load, model_build, model_train, model_test
 
 if __name__ == '__main__':
-    train = False
+    train = True
     test = not train
-    all_input_, all_adj_, all_output_ = data_load(new_data=True, embed_level='graph', embed_encoder=encoder_bert)
+    all_input_, all_adj_, all_output_ = data_load(
+        new_data=True,
+        embed_level='graph',
+        embed_encoder=encoder_bert,
+        data_folder='BertGCN'
+    )
     if os.path.exists('ScorePredictor.h5'):
         model_ = tf.keras.models.load_model('ScorePredictor.h5', custom_objects={'GraphConv': GraphConv})
     else:
